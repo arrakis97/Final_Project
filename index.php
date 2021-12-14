db3bfe37428a3c39afac9f78110b669032a5bfe3
arrakis97 <hjlindemulder@msn.com>
2021-12-14 17:57:58 +0100

    Added some standard stuff from the assignments

--- index.php
+++ index.php
@@ -2,3 +2,29 @@
 /**
  * Controller
  */
+
+/* Require composer autoloader */
+require __DIR__ . '/vendor/autoload.php';
+
+/* Include model.php */
+include 'model.php';
+
+/* Connect to DB */
+$db = connect_db('localhost', 'final_project', 'ddwt21', 'ddwt21');
+
+/* Create Router instance */
+$router = new \Bramus\Router\Router();
+
+
+
+
+
+
+/* Give a custom error message for the 404 error */
+$router->set404(function () {
+    header('HTTP/1.1 404 Not Found');
+    echo "404 error: page was not found.";
+});
+
+/* Run the router */
+$router->run();
--- model.php
+++ model.php
@@ -2,3 +2,32 @@
 /**
  * Model
  */
+
+/* Enable error reporting */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/**
+ * Connects to the database using PDO
+ * @param string $host Database host
+ * @param string $db Database name
+ * @param string $user Database user
+ * @param string $pass Database password
+ * @return PDO Database object
+ */
+function connect_db($host, $db, $user, $pass){
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $e) {
+        echo sprintf("Failed to connect. %s",$e->getMessage());
+    }
+    return $pdo;
+}
