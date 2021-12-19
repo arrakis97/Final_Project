0d48e4e0227882bb069927127de36ab4eb0973b9
arrakis97 <hjlindemulder@msn.com>
2021-12-19 13:52:45 +0100

    Stopped using bramus router, because it didn't work for me for some reason. Added navigation code from assignment 2

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
  *
  * Database-driven Webtechnology Final Project
  * Heine Jan Lindemulder
+ * Based on code from the assignments
  */
-
-/* Require composer autoloader */
-require __DIR__ . '/vendor/autoload.php';
 
 /* Include model.php */
 include 'model.php';
@@ -15,19 +13,50 @@ include 'model.php';
 /* Connect to DB */
 $db = connect_db('localhost', 'final_project', 'ddwt21', 'ddwt21');
 
-/* Create Router instance */
-$router = new \Bramus\Router\Router();
+/* Array with all the standard views */
+$navigation_array = Array (
+    1 => Array (
+        'name' => 'Home',
+        'url' => '/Final_Project/'
+    ),
+    2 => Array (
+        'name' => 'Overview',
+        'url' => '/Final_Project/overview/'
+    ),
+    3 => Array (
+        'name' => 'Add series',
+        'url' => '/Final_Project/add/'
+    ),
+    4 => Array (
+        'name' => 'My Account',
+        'url' => '/Final_Project/myaccount/'
+    ),
+    5 => Array (
+        'name' => 'Registration',
+        'url' => '/Final_Project/register/'
+    )
+);
+
+/* Home page */
+if (new_route('/Final_Project/', 'get')) {
+    /* Page info */
+    $page_title = "Home";
+    $breadcrumbs = get_breadcrumbs([
+        'Final Project' => na('/Final_Project/', False),
+        'Home' => na('/Final_Project', True)
+    ]);
+    /* Check which page is the active page */
+    $navigation = get_navigation($navigation_array, 1);
+
+    /* Page content */
+    $page_subtitle = 'The online platform to list your favorite series';
+    $page_content = 'On Series Overview you can list your favorite series. You can see the favorite series of all Series Overview users. By sharing your favorite series, you can get inspired by others and explore new series.';
+
+    /* Check if an error message is set and display it if available */
+    if (isset($_GET['error_msg'])) {
+        $error_msg = get_error($_GET['error_msg']);
+    }
 
 
-
-
-
-
-/* Give a custom error message for the 404 error */
-$router->set404(function () {
-    header('HTTP/1.1 404 Not Found');
-    echo "404 error: page was not found.";
-});
-
-/* Run the router */
-$router->run();
+    include use_template('main');
+}
--- model.php
+++ model.php
@@ -4,6 +4,7 @@
  *
  * Database-driven Webtechnology Final Project
  * Heine Jan Lindemulder
+ * Based on code from the assignments
  */
 
 /* Enable error reporting */
@@ -33,4 +34,128 @@ function connect_db($host, $db, $user, $pass){
         echo sprintf("Failed to connect. %s",$e->getMessage());
     }
     return $pdo;
+}
+
+/**
+ * Check if the route exists
+ * @param string $route_uri URI to be matched
+ * @param string $request_type Request method
+ * @return bool
+ */
+function new_route($route_uri, $request_type){
+    $route_uri_expl = array_filter(explode('/', $route_uri));
+    $current_path_expl = array_filter(explode('/',parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+    if ($route_uri_expl == $current_path_expl && $_SERVER['REQUEST_METHOD'] == strtoupper($request_type)) {
+        return True;
+    } else {
+        return False;
+    }
+}
+
+/**
+ * Creates a new navigation array item using URL and active status
+ * @param string $url The URL of the navigation item
+ * @param bool $active Set the navigation item to active or inactive
+ * @return array
+ */
+function na($url, $active){
+    return [$url, $active];
+}
+
+/**
+ * Creates filename to the template
+ * @param string $template Filename of the template without extension
+ * @return string
+ */
+function use_template($template){
+    $template_doc = sprintf("views/%s.php", $template);
+    return $template_doc;
+}
+
+/**
+ * Creates breadcrumb HTML code using given array
+ * @param array $breadcrumbs Array with as Key the page name and as Value the corresponding URL
+ * @return string HTML code that represents the breadcrumbs
+ */
+function get_breadcrumbs($breadcrumbs) {
+    $breadcrumbs_exp = '
+    <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">';
+    foreach ($breadcrumbs as $name => $info) {
+        if ($info[1]){
+            $breadcrumbs_exp .= '<li class="breadcrumb-item active" aria-current="page">'.$name.'</li>';
+        }else{
+            $breadcrumbs_exp .= '<li class="breadcrumb-item"><a href="'.$info[0].'">'.$name.'</a></li>';
+        }
+    }
+    $breadcrumbs_exp .= '
+    </ol>
+    </nav>';
+    return $breadcrumbs_exp;
+}
+
+/**
+ * Creates navigation HTML code using given array
+ * @param $template Array with page names and URL's
+ * @param $active_id ID of the current active page
+ * @return string HTML code that represents the navigation
+ */
+function get_navigation($template, $active_id){
+    $navigation_exp = '
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">Series Overview</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">';
+    foreach ($template as $name => $info) {
+        if ($name == $active_id){
+            $navigation_exp .= '<li class="nav-item active">';
+            $navigation_exp .= '<a class="nav-link" href="'.$info['url'].'">'.$info['name'].'</a>';
+        }else{
+            $navigation_exp .= '<li class="nav-item">';
+            $navigation_exp .= '<a class="nav-link" href="'.$info['url'].'">'.$info['name'].'</a>';
+        }
+
+        $navigation_exp .= '</li>';
+    }
+    $navigation_exp .= '
+    </ul>
+    </div>
+    </nav>';
+    return $navigation_exp;
+}
+
+/**
+ * Changes the HTTP Header to a given location
+ * @param string $location Location to redirect to
+ */
+function redirect($location){
+    header(sprintf('Location: %s', $location));
+    die();
+}
+
+/**
+ * Creates HTML alert code with information about the success or failure
+ * @param array $feedback Associative array with keys type and message
+ * @return string
+ */
+function get_error($feedback){
+    $feedback = json_decode($feedback, true);
+    $error_exp = '
+        <div class="alert alert-'.$feedback['type'].'" role="alert">
+            '.$feedback['message'].'
+        </div>';
+    return $error_exp;
+}
+
+/**
+ * Pretty Print Array
+ * @param $input
+ */
+function p_print($input){
+    echo '<pre>';
+    print_r($input);
+    echo '</pre>';
 }
