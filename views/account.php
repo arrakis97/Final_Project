dc8b38689e5589fc1d2ca1b73c2122fdc5c526a9
arrakis97 <hjlindemulder@msn.com>
2022-01-17 18:47:56 +0100

    Added the correct CSS

--- views/account.php
+++ views/account.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Own CSS -->
-    <link rel="stylesheet" href="/DDWT21/week2/css/main.css">
+    <link rel="stylesheet" href="/DDWT21/Final_Project/css/main.css">
 
     <title><?= $page_title ?></title>
 </head>
